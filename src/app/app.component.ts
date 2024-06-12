bd3e788e5313766a6fad1d9b4045aad79005ffab
= <=>
2024-06-11 21:07:39 -0500

    Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, UserComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styles: ``,
 })
 export class AppComponent {
   title = 'angular-17-app';
+  city = 'Peru';
 }
---
+++ src/app/games/games.component.ts
@@ -0,0 +1,44 @@
+import { Component, EventEmitter, Input, Output, output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-games',
+  standalone: true,
+  imports: [CommonModule, GamesComponent],
+  template: `
+    <h3>Los juegos favoritos de  {{username }} </h3>
+    <ul>
+      @for (game of games; track game.id) {
+        <li (click)= "fav(game.name)"> {{ game.name }}></li>
+      }
+    </ul>
+  `,
+  styles: ``,
+})
+export class GamesComponent {
+
+  @Input() username = '';
+  @Output() addFavoriteEvent = new EventEmitter<string>();
+
+  fav(gameName: string) {
+    this.addFavoriteEvent.emit(gameName);
+  }
+  games = [
+    { 
+      id: 1,
+      name: 'Uncharted 4'
+    },
+
+    {
+      id: 2,
+      name: 'Horizon Zero Dawn'
+    },
+
+    {
+      id:3,
+      name: 'free fire'
+    }
+
+  ]
+
+}
